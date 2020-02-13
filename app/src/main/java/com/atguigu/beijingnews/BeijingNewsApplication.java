d9c9e953e30f2ccb457cf5c66a5ea6249c117099
Jackqgm <724645516@qq.com>
2020-02-14 01:02:07 +0800

    2020-2-13 23:25 101.极光推送集成到项目中

--- app/src/main/java/com/atguigu/beijingnews/BeijingNewsApplication.java
+++ app/src/main/java/com/atguigu/beijingnews/BeijingNewsApplication.java
@@ -6,6 +6,8 @@ import com.atguigu.beijingnews.volley.VolleyManager;
 
 import org.xutils.x;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by Jackqgm on 2020/2/1.
  * 代表整个软件
@@ -22,5 +24,7 @@ public class BeijingNewsApplication extends Application {
 
         VolleyManager.init(this);
 
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
     }
 }
